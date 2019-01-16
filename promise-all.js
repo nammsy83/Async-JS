c19418dffbed32a32fb57695366c4c1f45f8f22c
Namrata Subhedar <nammsy@gmail.com>
2019-01-16 13:40:03 +0530

    added async, await, callbacks and promises logic examples

---
+++ promise-all.js
@@ -0,0 +1,8 @@
+// Promise.all
+
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => 
+	setTimeout(resolve, 2000, 'Goodbye'));
+const promise4 = fetch('https://reqres.in/api/users?page=2').then(res => res.json());
+Promise.all([promise1, promise2, promise3, promise4]).then((values => console.log(values)));
